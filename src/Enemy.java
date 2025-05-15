72eb40c9a9c73d4bd47aee9989cc5c7b59b3c592
markm6 <145475856+markm6@users.noreply.github.com>
2025-05-15 10:19:38 -0400

    Add files via upload

--- src/Enemy.java
+++ src/Enemy.java
@@ -3,26 +3,18 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
-public class Player {
+public class Enemy {
     private final int MOVE_AMT = 3;
-    private BufferedImage right;
-    private BufferedImage left;
-    private boolean facingRight;
+    private BufferedImage image;
     private int xCoord;
     private int yCoord;
-    private int score;
-    private String name;
-    public Player(String name) {
-        this.name = name;
-        facingRight = true;
-        xCoord = 50; // starting position is (50, 435), right on top of ground
-        yCoord = 435;
-        score = 0;
+
+    public Enemy(){
+        xCoord = 50;
+        yCoord = 100;
         try {
-            left = ImageIO.read(new File("src\\" + name + "left.png"));
-            right = ImageIO.read(new File("src\\" + name + "right.png"));
-        } catch (IOException e) {
+            image = ImageIO.read(new File("src\\goomba.png"));
+        } catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
@@ -34,19 +26,6 @@ public class Player {
     public int getyCoord() {
         return yCoord;
     }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void faceRight() {
-        facingRight = true;
-    }
-
-    public void faceLeft() {
-        facingRight = false;
-    }
-
     public void moveRight() {
         if (xCoord + MOVE_AMT <= 920) {
             xCoord += MOVE_AMT;
@@ -79,23 +58,14 @@ public class Player {
             xCoord += x;
         }
     }
-    public void collectCoin() {
-        score++;
-    }
-
-    public BufferedImage getPlayerImage() {
-        if (facingRight) {
-            return right;
-        } else {
-            return left;
-        }
-    }
-
-    // we use a "bounding Rectangle" for detecting collision
     public Rectangle playerRect() {
-        int imageHeight = getPlayerImage().getHeight();
-        int imageWidth = getPlayerImage().getWidth();
+        int imageHeight = image.getHeight();
+        int imageWidth = image.getWidth();
         Rectangle rect = new Rectangle(xCoord, yCoord, imageWidth, imageHeight);
         return rect;
+    }
+
+    public BufferedImage getImage() {
+        return image;
     }
 }
--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -11,18 +11,23 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
     private BufferedImage background;
     private Timer timer;
     private Player player;
+    private Player player2;
+    private Color textColor;
     private boolean[] pressedKeys;
     private ArrayList<Coin> coins;
-
+    private Enemy enemy;
     public GraphicsPanel() {
+        enemy = new Enemy();
         timer = new Timer(2, this);
+        textColor = Color.BLACK;
         timer.start();
         try {
             background = ImageIO.read(new File("src/background.png"));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        player = new Player();
+        player = new Player("mario");
+        player2 = new Player("luigi");
         coins = new ArrayList<>();
         pressedKeys = new boolean[128]; // 128 keys on keyboard, max keycode is 127
         addKeyListener(this);
@@ -37,7 +42,8 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
         // the order that things get "painted" matter; we paint the background first
         g.drawImage(background, 0, 0, null);
         g.drawImage(player.getPlayerImage(), player.getxCoord(), player.getyCoord(), null);
-
+        g.drawImage(player2.getPlayerImage(), player2.getxCoord(), player2.getyCoord(), null);
+        g.drawImage(enemy.getImage(), enemy.getxCoord(), enemy.getyCoord(), null);
         // this loop does two things:  it draws each Coin that gets placed with mouse clicks,
         // and it also checks if the player has "intersected" (collided with) the Coin, and if so,
         // the score goes up and the Coin is removed from the arraylist
@@ -49,12 +55,18 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
                 coins.remove(i);
                 i--;
             }
+            if (player2.playerRect().intersects(coin.coinRect())) { // check for collision
+                player2.collectCoin();
+                coins.remove(i);
+                i--;
+            }
         }
 
         // draw score
         g.setFont(new Font("Courier New", Font.BOLD, 24));
+        g.setColor(textColor);
         g.drawString("Score: " + player.getScore(), 20, 40);
-
+        g.drawString("Score: " + player2.getScore(), 800, 40);
         // player moves left (A)
         if (pressedKeys[65]) {
             player.faceLeft();
@@ -76,6 +88,21 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
         if (pressedKeys[83]) {
             player.moveDown();
         }
+
+        if (pressedKeys[38]) {
+            player2.moveUp();
+        }
+        if (pressedKeys[40]) {
+            player2.moveDown();
+        }
+        if (pressedKeys[37]) {
+            player2.faceLeft();
+            player2.moveLeft();
+        }
+        if (pressedKeys[39]) {
+            player2.faceRight();
+            player2.moveRight();
+        }
     }
 
     // ActionListener interface method
@@ -94,6 +121,7 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
         // see this for all keycodes: https://stackoverflow.com/questions/15313469/java-keyboard-keycodes-list
         // A = 65, D = 68, S = 83, W = 87, left = 37, up = 38, right = 39, down = 40, space = 32, enter = 10
         int key = e.getKeyCode();
+        System.out.println(key);
         pressedKeys[key] = true;
     }
 
@@ -114,16 +142,38 @@ public class GraphicsPanel extends JPanel implements ActionListener, KeyListener
 
     @Override
     public void mouseReleased(MouseEvent e) {
+
         if (e.getButton() == MouseEvent.BUTTON1) {  // left mouse click
             Point mouseClickLocation = e.getPoint();
             Coin coin = new Coin(mouseClickLocation.x, mouseClickLocation.y);
             coins.add(coin);
         }
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            Point mouseClickLocation = e.getPoint();
+            double slope = (double) (mouseClickLocation.y - player.getyCoord()) / (mouseClickLocation.x - player.getxCoord());
+            int x = 50;
+            if (mouseClickLocation.x < player.getxCoord()) {
+                x -= 100;
+            }
+            int y = (int) (slope * x);
+            player.move(x, y);
+            double slope2 = (double) (mouseClickLocation.y - player2.getyCoord()) / (mouseClickLocation.x - player2.getxCoord());
+            int x2 = 50;
+            if (mouseClickLocation.x < player2.getxCoord()) {
+                x2 -= 100;
+            }
+            int y2 = (int) (slope2 * x2);
+            player2.move(x2, y2);
+        }
     }
 
     @Override
-    public void mouseEntered(MouseEvent e) { } // unimplemented
+    public void mouseEntered(MouseEvent e) {
+        textColor = Color.BLACK;
+    } // unimplemented
 
     @Override
-    public void mouseExited(MouseEvent e) { } // unimplemented
+    public void mouseExited(MouseEvent e) {
+        textColor = Color.RED;
+    } // unimplemented
 }
